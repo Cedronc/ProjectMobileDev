f797e17f23646145537b50a93f8923b483716bff
Cedric <cedricmrtl@gmail.com>
2022-10-25 11:29:08 +0200

    init

---
+++ app/src/main/java/com/example/projectmobiledev/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.projectmobiledev
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val database = Firebase.database("https://mobiledevproject-e36ca-default-rtdb.europe-west1.firebasedatabase.app/")
+        val myRef = database.getReference("features")
+
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = dataSnapshot.value
+                Log.d("Testing", "Value is: $value")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+                Log.w("Testing", "Failed to read value.", error.toException())
+            }
+        })
+
+
+    }
+}
---
+++ app/src/main/java/com/example/projectmobiledev/PublicToilet.kt
@@ -0,0 +1,4 @@
+package com.example.projectmobiledev
+
+class PublicToilet() {
+}
