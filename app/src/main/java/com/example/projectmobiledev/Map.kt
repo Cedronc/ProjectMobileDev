a0388898b77af4ed45979cc4c4d369f3532d943d
DriesLeyers2002 <dries@dipu.com>
2022-11-22 10:46:55 +0100

    Added marker in Sydney

---
+++ app/src/main/java/com/example/projectmobiledev/Map.kt
@@ -0,0 +1,31 @@
+package com.example.projectmobiledev
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class Map : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+    }
+
+  override fun onMapReady(googleMap: GoogleMap) {
+    val sydney = LatLng(-33.852, 151.211)
+    googleMap.addMarker(
+      MarkerOptions()
+        .position(sydney)
+        .title("Marker in Sydney")
+    )
+    googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+  }
+
+}
