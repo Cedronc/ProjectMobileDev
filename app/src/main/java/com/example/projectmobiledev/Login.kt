035cd05e598fbfcb59697ee8bcd2327e73dd897b
Cedric <cedricmrtl@gmail.com>
2022-12-09 02:33:15 +0100

    Added todo

--- app/src/main/java/com/example/projectmobiledev/Login.kt
+++ app/src/main/java/com/example/projectmobiledev/Login.kt
@@ -74,10 +74,9 @@ class Login : AppCompatActivity() {
           val user: FirebaseUser = auth.getCurrentUser()!!
           if (user.isEmailVerified) {
             uid = auth.getCurrentUser()!!.getUid()
-            val mainActivity = Intent(
-              this@Login,
-              MainActivity::class.java
-            ) //We maken een nieuwe intent dat van de register pagina terug naar de login pagina laat gaan
+            //We maken een nieuwe intent dat van de register pagina terug naar de login pagina laat gaan
+            //TODO add user to intent or other way to get user to MainActivity
+            val mainActivity = Intent(this@Login,MainActivity::class.java)
             mainActivity.putExtra("UID", uid)
             startActivity(mainActivity) //We starten de intent
           } else {
