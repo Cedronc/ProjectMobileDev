323513a7a6748798a97e9d13bdea4a35f19b88d1
DriesLeyers2002 <dries@dipu.com>
2022-12-09 01:03:34 +0100

    add

--- app/src/main/java/com/example/projectmobiledev/Register.kt
+++ app/src/main/java/com/example/projectmobiledev/Register.kt
@@ -1,11 +1,41 @@
 package com.example.projectmobiledev
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
 
 class Register : AppCompatActivity() {
+  var reff: DatabaseReference? = null
+  private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+      super.onCreate(savedInstanceState)
+      setContentView(R.layout.activity_register)
+
+      mAuth = FirebaseAuth.getInstance()
+
+      val already_have_an_account = findViewById<View>(R.id.already_have_account) as Button
+
+      already_have_an_account.setOnClickListener {
+        val registerActivity = Intent(
+          this@Register,
+          Login::class.java
+        )
+        startActivity(registerActivity)
+      }
+
+      val register_button = findViewById<View>(R.id.register_button) as Button
+      val firstName = findViewById<View>(R.id.firstName_registration) as EditText
+      val lastName = findViewById<View>(R.id.lastName_registration) as EditText
+      val email = findViewById<View>(R.id.email_register) as EditText
+      val password = findViewById<View>(R.id.password_registration) as EditText
+      val c_password = findViewById<View>(R.id.confirm_password_registration) as EditText
+
     }
 }
