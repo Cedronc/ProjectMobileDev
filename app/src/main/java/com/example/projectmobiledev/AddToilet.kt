6787f3e6a2d5cb670b6fc4453e7111f09d1f17e1
Cedric <cedricmrtl@gmail.com>
2022-12-12 16:55:21 +0100

    UUID added

--- app/src/main/java/com/example/projectmobiledev/AddToilet.kt
+++ app/src/main/java/com/example/projectmobiledev/AddToilet.kt
@@ -4,15 +4,33 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class AddToilet : AppCompatActivity() {
+
+    val firebaseDb = Firebase.database("https://mobiledevproject-e36ca-default-rtdb.europe-west1.firebasedatabase.app/")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_toilet)
 
 
 
-        
+        findViewById<Button>(R.id.push_toilet_btn).setOnClickListener {
+            //TODO: push toilet to database
+
+            val newUUID = UUID.randomUUID().toString()
+            setNewUUIDFirebase(newUUID)
+        }
+    }
+
+    private fun setNewUUIDFirebase(newUUID: String) {
+        val database = firebaseDb.reference
+        database.child("ToiletUUID").setValue(newUUID)
     }
 
     private fun saveUUID() {
